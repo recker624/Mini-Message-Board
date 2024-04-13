6941636ac87bc2d31f47d290658a76ca44523411
Sahil saini <52703428+recker624@users.noreply.github.com>
2024-04-13 09:47:49 +0530

    added form to create messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World",
+    user: "Charles",
+    added: new Date(),
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { messages });
+});
+
+router.get('/new', (req, res, next) => {
+   res.render('form');
+});
+
+router.post("/new", (req, res, next) => {
+  const user = req.body.user;
+  const message = req.body.message;
+
+  messages.push({ text: message, user: user, added: new Date() });
+  res.redirect('/');
 });
 
 module.exports = router;
